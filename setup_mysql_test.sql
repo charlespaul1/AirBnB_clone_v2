50558e8a3052ccfdb2ac608147aae0be8482d1e1
charlespaul1 <charlespaulwabomba@gmail.com>
2023-01-06 23:03:53 +0300

    fix

--- setup_mysql_test.sql
+++ setup_mysql_test.sql
@@ -1,13 +1,15 @@
--- creating test user n the mysql server & giving it permissions
+-- setup the db test env
 
 CREATE DATABASE IF NOT EXISTS hbnb_test_db;
-GRANT USAGE ON *.*
-      TO 'hbnb_test'@'localhost'
-      IDENTIFIED BY 'hbnb_test_pwd';
-GRANT ALL PRIVILEGES ON hbnb_test_db.*
-      TO 'hbnb_test'@'localhost'
-      IDENTIFIED BY 'hbnb_test_pwd';
-GRANT SELECT ON performance_schema.*
-      TO 'hbnb_test'@'localhost'
-      IDENTIFIED BY 'hbnb_test_pwd';
+CREATE USER
+    IF NOT EXISTS 'hbnb_test'@'localhost'
+    IDENTIFIED BY 'hbnb_test_pwd';
+GRANT ALL PRIVILEGES
+   ON `hbnb_test_db`.*
+   TO 'hbnb_test'@'localhost'
+   IDENTIFIED BY 'hbnb_test_pwd';
+GRANT SELECT
+   ON `performance_schema`.*
+   TO 'hbnb_test'@'localhost'
+   IDENTIFIED BY 'hbnb_test_pwd';
 FLUSH PRIVILEGES;
