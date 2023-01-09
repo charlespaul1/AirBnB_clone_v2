85694aff61137db1c91f74af4c42fb8e445bdd5a
charlespaul1 <charlespaulwabomba@gmail.com>
2023-01-09 12:04:26 +0300

    db storage

--- models/__init__.py
+++ models/__init__.py
@@ -1,14 +1,14 @@
 #!/usr/bin/python3
 """This module instantiates an object of class FileStorage"""
-import os
-from models.engine.db_storage import DBStorage
-from models.engine.file_storage import FileStorage
+from os import getenv
 
-storage_type = os.environ.get('HBNB_TYPE_STORAGE')
 
-if storage_type == 'db':
+if getenv("HBNB_TYPE_STORAGE") == "db":
+    from models.engine.db_storage import DBStorage
+
     storage = DBStorage()
 else:
-    storage = FileStorage()
+    from models.engine.file_storage import FileStorage
 
+    storage = FileStorage()
 storage.reload()
--- models/state.py
+++ models/state.py
@@ -1,22 +1,29 @@
 #!/usr/bin/python3
 """ State Module for HBNB project """
-from models.base_model import BaseModel, Base
-from sqlalchemy import Column, String
+from models.base_model import BaseModel
+from models.base_model import Base
+from sqlalchemy import Column
+from sqlalchemy import String
 from sqlalchemy.orm import relationship
-from models.city import City
 from os import getenv
+import models
+from models.city import City
 
 
 class State(BaseModel, Base):
-    """ State class """
-    __tablename__ = 'states'
+    """State class"""
+
+    __tablename__ = "states"
     name = Column(String(128), nullable=False)
+    cities = relationship("City", backref="state", cascade="delete")
 
-    cities = relationship("City", cascade="all, delete, delete-orphan")
+    if getenv("HBNB_TYPE_STORAGE") != "db":
 
-    @property
-    def cities(self):
-        """get all  cities related"""
-        from models import storage
-        cities = storage.all("City").values()
-        return [city for city in cities if city.state_id == self.id]
+        @property
+        def cities(self):
+            """Get all cities related"""
+            c_list = []
+            for city in list(models.storage.all(City).values()):
+                if city.state_id == self.id:
+                    c_list.append(city)
+            return c_list
